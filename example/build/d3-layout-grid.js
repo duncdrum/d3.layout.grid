4f57d3d27f2bc8b33640bee945d85e36f934b098
Mikkel B. Jensen <mikkel.jensen@eb.dk>
2017-04-24 16:31:18 +0200

    Added build

--- example/build/d3-layout-grid.js
+++ example/build/d3-layout-grid.js
@@ -7,8 +7,7 @@
 /*eslint-disable */
 {
   document.write(
-    '<script src="http://' + (location.host || 'localhost')
-    .split(':')[0] + ':35729/livereload.js?snipver=1"></script>'
+    '<script src="http://' + (location.host || 'localhost').split(':')[0] +':35729/livereload.js?snipver=1"></script>'
   );
 }
 /*eslint-enable */
